7e64e71f335652fb43cea2ff0ecd6e375d9939fa
Alec Sawatzky <alecsawatzky@gmail.com>
2017-10-31 22:27:33 -0500

    Add order_id reference to LineItems table and migrate the change.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101012807) do
+ActiveRecord::Schema.define(version: 20171101032420) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20171101012807) do
     t.decimal "item_discount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
