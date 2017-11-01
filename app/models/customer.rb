f664170d660990b3f704b0b233e45d586c832d08
Alec Sawatzky <alecsawatzky@gmail.com>
2017-10-31 22:37:26 -0500

    Revert "Added associations for all the models."
    This reverts commit 21c33f7591b0164d60243960f863f6806a9d17a3.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,3 @@
 class Customer < ApplicationRecord
-    has_many :orders
     validates :stripe_customer_id, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,3 @@
 class Order < ApplicationRecord
-    has_many :line_items
-    belongs_to :customer
     validates :satus, :pst_rate, :gst_rate, :hst_rate, :customer_id, presence: true
 end
