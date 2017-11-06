feae179c49ae8082caf1866f5ef28d21e29f5f00
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-05 19:39:12 -0600

    Added name uniqueness for Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
     has_one :sale_product
     has_many :line_items
     validates :name, :description, :price, :stock_quantity, :category_id, presence: true    
-
+    validates :name, uniqueness: {message: "An existing product has the same name." }
     mount_uploader :image, AvatarUploader
-    
 end
