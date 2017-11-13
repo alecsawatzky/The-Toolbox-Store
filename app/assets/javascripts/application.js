3925d94ef489e59df2d1691ff7182d926f266be5
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-13 13:07:43 -0600

    Added jquery gem.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery_ujs
