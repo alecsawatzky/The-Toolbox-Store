3e4308b094f990de9d759142b6ffb5f3db7cc595
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-20 20:17:25 -0600

    Add Stripe payment processing.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :charges, only: [:new, :create]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
