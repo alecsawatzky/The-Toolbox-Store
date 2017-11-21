df4567ee0fad3e3045b6ad37f8027606b90384c8
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-20 21:11:47 -0600

    Add Name column to Customers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121022131) do
+ActiveRecord::Schema.define(version: 20171121031108) do
 
   create_table "Products", force: :cascade do |t|
     t.string "name"
