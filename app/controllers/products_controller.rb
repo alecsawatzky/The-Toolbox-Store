ce83650f7b8e46483e0f23b78a91dad5ccb6300d
Alec Sawatzky <alecsawatzky@gmail.com>
2017-11-25 20:13:20 -0600

    Fix small itemes. Cleanup code.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,19 @@
 class ProductsController < ApplicationController
-  
+
   before_action :initialize_session, :intialize_variables
 
   def index
     @products = Product.all
-    @sale_items = SaleProduct.all
   end
 
   def show
     @product = Product.find(params[:id])
-    @sale_items = SaleProduct.all
   end
 
   def cart
     customer_details
     @items_in_cart = Product.find(session[:cart])
     @product_id_list = session[:cart]
-
-    @sale_items = SaleProduct.all
   end
 
   def checkout
@@ -26,12 +22,10 @@ class ProductsController < ApplicationController
   end
 
   def sale
-    @sale_items = SaleProduct.all
   end
 
   def new
     @new_items = Product.where(:new => true)
-    @sale_items = SaleProduct.all
   end
 
   def search
@@ -46,20 +40,21 @@ class ProductsController < ApplicationController
 
   def filter
     if params[:commit] == "Filter"
+      @category = Category.find(params[:filter])
       @filter_results = Product.where("category_id == '#{params[:filter]}'").order(:name)
     else
       redirect_to root_url
     end
   end
 
-   def add_to_cart
-    id = params[:id].to_i;
+  def add_to_cart
+    id = params[:id].to_i
     session[:cart] << id
     redirect_to cart_url
     flash[:notice] = "1 item added to cart"
-   end
+  end
 
-   def edit_quantity
+  def edit_quantity
     id = params[:id].to_i
 
     if params[:commit] == "+"
@@ -67,14 +62,14 @@ class ProductsController < ApplicationController
     elsif params[:commit] == "-"
       if session[:cart].count(id) > 1
         session[:cart].delete_at(session[:cart].index(id) || session[:cart].length)
-      end  
+      end
     else
       session[:cart].delete(id)
     end
     redirect_to cart_url
-   end
+  end
 
-  private 
+  private
 
   def initialize_session
     session[:cart] ||= []
@@ -89,6 +84,24 @@ class ProductsController < ApplicationController
   end
 
   def customer_details
+    @total_items_in_cart = session[:cart].map{ |id| Product.order(:name).find(id) }
+    @total_items_in_cart =  @total_items_in_cart.sort_by(&:name)
+    @total = @total_items_in_cart.sum(&:price) * 100
+    @subtotal = 0
+    @amount_saved = 0
+
+    @total_items_in_cart.each do |product|
+      price = product.price
+      if SaleProduct.find_by_id(product.id).present?
+        discount_decimal = (SaleProduct.find(product.id).discount_percent / 100) * price
+        price = price - discount_decimal
+        @amount_saved += discount_decimal
+      end
+      @subtotal += price
+    end
+
+    @subtotal = @subtotal * 100
+
     if customer_signed_in?
       @current_customer = current_customer
       @province = @current_customer.province
@@ -96,18 +109,17 @@ class ProductsController < ApplicationController
       @pst = @province.pst
       @gst = @province.gst
       @hst = @province.hst
-    else 
+
+      @sales_pst = (@subtotal * @pst).round()
+      @sales_gst = (@subtotal * @gst).round()
+      @sales_hst = (@subtotal * @hst).round()
+      @grand_total = (@subtotal + (@sales_gst + @sales_pst + @sales_hst)).round()
+    else
       @pst = 0
       @gst = 0
       @hst = 0
+      @grand_total = 0
     end
-    @total_items_in_cart = session[:cart].map{ |id| Product.order(:name).find(id) }
-    @total_items_in_cart =  @total_items_in_cart.sort_by(&:name)
-    @subtotal = @total_items_in_cart.sum(&:price) * 100
-    @sales_pst = (@subtotal * @pst).round()
-    @sales_gst = (@subtotal * @gst).round()
-    @sales_hst = (@subtotal * @hst).round()
-    @grand_total = (@subtotal + (@sales_gst + @sales_pst + @sales_hst)).round()
   end
 
   def customer_signed_in?
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
     has_many :line_items
     belongs_to :customer
     validates :pst_rate, :gst_rate, :hst_rate, :customer_id, presence: true
-
-    def name
-        id
-    end
 end
